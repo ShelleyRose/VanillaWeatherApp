7d2d325b9191329109f17ca43fe98e2c5ab33b95
ShelleyRose <56639714+ShelleyRose@users.noreply.github.com>
2024-04-12 14:15:27 +0100

    Working on adding apiUrl and apiKey

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
+function searchCity(city) {
+  let apiKey = "6007ec7ao2bb33948f4d6298t91b3316";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
+  console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+//call API
+//search for city
